ab3c3d708ff11a050c6c4ab0856dad03d6482d93
acbgf8 <acbgf8@mail.missouri.edu>
2021-03-03 22:24:06 -0600

    Update carstate.py

--- selfdrive/car/chrysler/carstate.py
+++ selfdrive/car/chrysler/carstate.py
@@ -10,8 +10,8 @@ class CarState(CarStateBase):
   def __init__(self, CP):
     super().__init__(CP)
     can_define = CANDefine(DBC[CP.carFingerprint]['pt'])
-    print("SHIFTER_ASSM")
-    print("SHIFTER_POSITION")
+    print(can_define.dv["SHIFTER_ASSM"]['SHIFTER_POSITION'])
+    print(can_define)
     self.shifter_values = can_define.dv["SHIFTER_ASSM"]['SHIFTER_POSITION']
 
   def update(self, cp):
